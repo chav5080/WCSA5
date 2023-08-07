ac4fb67549a2e1d1b18ef068e597a6441be4bb6c
Shivaji <Shivaji@LAPTOP-5BSDQGDQ>
2023-08-07 11:37:33 +0530

    COmm

---
+++ TimeSheetManagementToolProject/src/main/java/qsp/Demo.java
@@ -0,0 +1,12 @@
+package qsp;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("RiP Maven");
+		WebDriver driver =new ChromeDriver();
+
+	}
+}
